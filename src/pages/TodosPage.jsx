47beaf8439033657dbd5f87eda7f45724692edd6
Vera869 <verea8910@gmail.com>
2023-12-23 16:09:28 +0700

    +catch getTodos

--- src/pages/TodosPage.jsx
+++ src/pages/TodosPage.jsx
@@ -4,11 +4,15 @@ import { deleteTodo, getTodos } from "../api";
 
 export default function TodosPage() {
   const [todos, setTodos] = useState([]);
+  const [addGetTodosError, setAddGetTodosError] = useState(null);
 
   useEffect(() => {
      getTodos().then((todos) => {
      setTodos(todos.todos);
-    });
+    }).catch((error) => {
+      setAddGetTodosError(error.message);
+      setTodos([]);
+    })
   },[])
   const [isTodoLoading, setIsTodoLoading] = useState(false);
   const [isDeleteLoading, setIsDeleteLoading] = useState();
@@ -29,6 +33,7 @@ export default function TodosPage() {
 
   return (
     <div className="page">
+      <p style={{color: "red"}}>{addGetTodosError}</p>
       <h1>Список задач</h1>
       <p style={{color: "red"}}>{addDeleteError}</p>
       <ul>
